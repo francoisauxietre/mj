57962884313d376a18e17761445d741076ea1208
f.Auxietre <f.Auxietre@gmail.com>
2020-01-21 14:17:15 +0100

    ajout de package angular browser

--- mjAngular/src/app/cordova.service.ts
+++ mjAngular/src/app/cordova.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NgZone } from '@angular/core';
 
 
-import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/map';
 import {BehaviorSubject, Observable} from 'rxjs';
 function _window(): any {
   // return the global native browser window object
@@ -10,24 +10,24 @@ function _window(): any {
 @Injectable()
 export class CordovaService {
 
-  private resume: BehaviorSubject<boolean>;
-  constructor(private zone: NgZone) {
-    this.resume = new BehaviorSubject<boolean>(null);
-    // avant on avait .formEvent de la librairie reactivex mais non installable
-    Observable.arguments(document, 'resume').subscribe(event => {
-      this.zone.run(() => {
-        this.onResume();
-      });
-    });
-  }
-
-  get cordova(): any {
-    return _window().cordova;
-  }
-  get onCordova(): boolean {
-    return !!_window().cordova;
-  }
-  public onResume(): void {
-    this.resume.next(true);
-  }
+  // private resume: BehaviorSubject<boolean>;
+  // constructor(private zone: NgZone) {
+  //   this.resume = new BehaviorSubject<boolean>(null);
+  //   // avant on avait .formEvent de la librairie reactivex mais non installable
+  //   Observable.arguments(document, 'resume').subscribe(event => {
+  //     this.zone.run(() => {
+  //       this.onResume();
+  //     });
+  //   });
+  // }
+  //
+  // get cordova(): any {
+  //   return _window().cordova;
+  // }
+  // get onCordova(): boolean {
+  //   return !!_window().cordova;
+  // }
+  // public onResume(): void {
+  //   this.resume.next(true);
+  // }
 }
--- mjAngular/src/app/time-line/time-line.component.ts
+++ mjAngular/src/app/time-line/time-line.component.ts
@@ -5,11 +5,52 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './time-line.component.html',
   styleUrls: ['./time-line.component.css']
 })
-export class TimeLineComponent implements OnInit {
+export class TimeLineComponent  {
 
   constructor() { }
+  alternate: boolean = true;
+  toggle: boolean = true;
+  color: boolean = false;
+  size: number = 40;
+  expandEnabled: boolean = true;
+  side = 'left';
 
-  ngOnInit() {
+  entries = [
+    {
+      header: 'header',
+      content: 'content'
+    }
+  ]
+
+  addEntry() {
+    this.entries.push({
+      header: 'header',
+      content: 'content'
+    })
+  }
+
+  removeEntry() {
+    this.entries.pop();
+  }
+
+  onHeaderClick(event) {
+    if (!this.expandEnabled) {
+      event.stopPropagation();
+    }
+  }
+
+  onDotClick(event) {
+    if (!this.expandEnabled) {
+      event.stopPropagation();
+    }
+  }
+
+  onExpandEntry(expanded, index) {
+    console.log(`Expand status of entry #${index} changed to ${expanded}`)
+  }
+
+  toggleSide() {
+    this.side = this.side === 'left' ? 'right' : 'left';
   }
 
 }
