b1ac4238b6aa39a5472b398063d6481207cc43d7
audrey <audreymartinai@gmail.com>
2020-01-22 09:14:54 +0100

    ajout Component generateur de dé

---
+++ mjAngular/src/app/generateur/generateur.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generateur',
+  templateUrl: './generateur.component.html',
+  styleUrls: ['./generateur.component.css']
+})
+export class GenerateurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  submitgenerateur(){
+
+  }
+
+}
