4bff5226b7a2d9c3a0262c6caad36ff1cc62d6cf
Hembert Guillaume <guillaume.hembert@etudiant.univ-rennes1.fr>
2020-01-28 14:17:13 +0100

    Ajout selection carte

--- mjAngular/src/app/carte/carte.component.ts
+++ mjAngular/src/app/carte/carte.component.ts
@@ -7,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarteComponent implements OnInit {
 
-  cheminImage: any = '../assets/Images/gantt1.png';
+  public listeCarte: any = [{value: 'carte1.jpg', name: 'carte 1'},
+    {value: 'carte2.jpeg', name: 'carte 2'}];
+  cheminImage: any = '';
 
   constructor() { }
 
   ngOnInit() {
+    this.cheminImage = '../assets/Images/' + this.listeCarte[0].value;
+  }
+
+  changeImage(selectedValue: string) {
+    this.cheminImage = '../assets/Images/' + selectedValue;
   }
 
 }
