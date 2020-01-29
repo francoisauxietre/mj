6b43a95203a06fa8e2e235d3c433585f52cba3c7
Etienne <etiene.imbert@wanadoo.fr>
2020-01-29 09:48:59 +0100

    cleaned generateur component file

--- mjAngular/src/app/generateur/generateur.component.ts
+++ mjAngular/src/app/generateur/generateur.component.ts
@@ -1,24 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-generateur',
-  templateUrl: './generateur.component.html',
-  styleUrls: ['./generateur.component.css']
-})
-@Component({
-  selector: 'app-generateur',
-  templateUrl: './generateur.component.html',
-  styleUrls: ['./generateur.component.css']
-})
 
+@Component({
+  selector: 'app-generateur',
+  templateUrl: './generateur.component.html',
+  styleUrls: ['./generateur.component.css']
+})
 export class GenerateurComponent implements OnInit {
 
-  listDe = [4,6,8,10,12,20,100,"Autre"];
+  listDe = [4, 6, 8, 10, 12, 20, 100, 'Autre'];
   total: number;
   selectDe: number;
-  nbDe: number = 1;
-  listValue : String ="";
-  listValueSorted = []
+  nbDe = 1;
+  listValue = '';
+  listValueSorted = [];
 
   constructor() {
 
@@ -27,39 +22,35 @@ export class GenerateurComponent implements OnInit {
   ngOnInit() {
   }
 
-  onSubmit(){
+  onSubmit() {
 
-    this.listValue ="";
+    this.listValue = '';
     this.listValueSorted = [];
-    if(this.nbDe!=null && this.selectDe!=null){
-      if(!isNaN(this.selectDe)){
-        
-      let i = this.nbDe;
-      this.total = 0;
-      this.listValue = "";
-      while(i>0){
-        let value = (Math.floor(Math.random()*this.selectDe)+1);
-        this.listValueSorted.push(value);
-        this.total = this.total + value;
-        i--;
-      }
-      this.listValueSorted.sort(function (a,b) { return a-b; });
-      this.listValueSorted.forEach(element => this.listValue += "[" + element.toString() + "] ");
-    }
-      else {
-        var number=window.prompt("Entrez une valeur de dé personnalisée:");
-        if(number==""){
-          window.alert("Veuillez choisir une taille de dé");
+    if (this.nbDe != null && this.selectDe != null) {
+      if (!isNaN(this.selectDe)) {
+        let i = this.nbDe;
+        this.total = 0;
+        this.listValue = '';
+        while (i > 0) {
+          const value = (Math.floor(Math.random() * this.selectDe) + 1);
+          this.listValueSorted.push(value);
+          this.total = this.total + value;
+          i--;
         }
-        else {
-        this.listDe.pop();
-        this.listDe.push(number);
-        this.listDe.push("Autre")
+        this.listValueSorted.sort((a, b) => a - b );
+        this.listValueSorted.forEach(element => this.listValue += '[' + element.toString() + '] ');
+      } else {
+        const num = window.prompt('Entrez une valeur de dé personnalisée:');
+        if (num === '') {
+          window.alert('Veuillez choisir une taille de dé');
+        } else {
+          this.listDe.pop();
+          this.listDe.push(num);
+          this.listDe.push('Autre');
         }
       }
-
     } else {
-      window.alert("Veuillez choisir une taille de dé");
+      window.alert('Veuillez choisir une taille de dé');
     }
 
   }
